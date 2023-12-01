81882938c4b15b5d6ef6d12d5ef65ac6445f0288
Unitoshka <unitoshka77@gmail.com>
2023-12-02 00:38:01 +0500

    first commit

---
+++ Main.py
@@ -0,0 +1,74 @@
+import httpx
+import time
+from enum import Enum
+import json
+
+with open('config.json', 'r', encoding='utf8') as f:
+    config = json.load(f)
+
+
+class CounterType(Enum):
+    Sympathies = 'user_like_count'
+    User_Messages = 'user_message_count'
+
+
+class Counter():
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.headers = {'Authorization': f'Bearer {self.token}',
+                                "accept": "application/json"}
+        self.baseUrl = "https://api.zelenka.guru"
+
+    def edit_message(self, message_id: int, post_body: str) -> None:
+        with httpx.Client(headers=self.headers) as client:
+            data = {"post_body": post_body}
+
+            request = client.put(url=f'{self.baseUrl}/profile-posts/{message_id}',
+                                 data=data)
+
+    def get_profile(self) -> dict:
+        with httpx.Client(headers=self.headers) as client:
+            request = client.get(url=f'{self.baseUrl}/users/me')
+        return request.json()
+
+    def get_list(self, counter_type: CounterType) -> list[int]:
+        profile = self.get_profile()
+        value = profile.get('user').get(counter_type.value)
+        split = [int(i) for i in str(value)]
+
+        return split
+
+    @staticmethod
+    def list_to_counter(split: list[int]) -> list[str]:
+        counter_list: list[str] = []
+
+        for number in split:
+            counter_list.append(f'[IMG]https://counter.unitoshka.fun/girls/{number}.png[/IMG]')
+
+        return counter_list
+
+class Updater():
+    @staticmethod
+    def get_updates() -> None:
+        pass
+
+
+
+if __name__ == '__main__':
+    counter = Counter(token=config['token'])
+
+    counter_type = CounterType[config['counter_type']]
+
+    while True:
+        list_ = counter.get_list(counter_type=counter_type)
+        counter_list = counter.list_to_counter(list_)
+
+        counter_text = ' '.join(counter_list)
+        message = str(config["message_text"]).replace("{counter}", counter_text)
+
+        time.sleep(10)
+
+        counter.edit_message(message_id=config['message_id'], post_body=message)
+        print('Counter.Unitoshka.fun -- Сообщение было изменено')
+
+        time.sleep(10)
